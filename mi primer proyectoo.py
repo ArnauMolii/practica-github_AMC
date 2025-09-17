c82fb3d5fd21ccca832e5aa86ce71ad2a166f55c
ArnauMoliner <arnau.moliner@campus.xaloc.org>
2025-09-17 09:21:01 +0200

    Add files via upload
    Mi primer proyecto. Arnau Moliner

---
+++ mi primer proyectoo.py
@@ -0,0 +1,37 @@
+#en este apartado estamos definiendo Variables
+var1="Xaloc"  #esta variable es de tipo string y como puedes obsrevar esta entre comillas
+var2=4        # esta variable contiene un velor entero
+var3=5
+var4=1.8      #esta variable contiene un valor de tipo float/decimal
+
+
+#esto no se hace   var 3= "no es correcto"
+
+
+print("Mi nombre es Arnau")   #sirve para que el mensaje que haya entre parentesis aparexca en el IDLE phyton.
+
+
+suma=var2+var3  #sirve para hacer una suma de dos variables diferentes.
+
+print(suma)     #aqui se pone directamente el resultado en el IDLE phyton
+
+print(var2+var3) #forma mas facil de hacer la suma.
+
+nombre=input("introduce tu nombre ")
+apellido1=input("introduce tu primer apellido ")
+apellido2=input("introduce tu segundo apellido")
+nombre_completo= nombre + " " + apellido1 + " " + apellido2
+print(nombre_completo)
+
+
+
+
+# OPERADORES ARITMETICOS EN PHYTON
+
+print(var2 + var3) 
+print(var2 - var3) 
+print(var2 * var3) 
+print(var2 / var3) 
+print(var2 % var3) 
+print(var2 ** var3) 
+print(var2 // var3) 
